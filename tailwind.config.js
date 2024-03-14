e5528ffada2f302c776303778fb44cc51d44fe6b
“diksom” <diki_somantri@ymail.com>
2024-03-14 10:41:45 +0800

    first-commit-diki-somantri

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#5B50E1",
+      },
+    },
+  },
+  plugins: [],
+};
